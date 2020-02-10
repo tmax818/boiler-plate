efc71a57eff93f3a257b15a4cd996da55102b9c4
tmax818 <tylermaxwell661@gmail.com>
2020-02-10 11:42:41 -0800

    add title and link

--- src/App.js
+++ src/App.js
@@ -6,17 +6,19 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Tyler's React Boiler Plate</h1>
+        <h2>Day One</h2>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.tylermaxwell.co"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn About Tyler
         </a>
       </header>
     </div>
